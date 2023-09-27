67355c95bdc985dc31246363babba6030f6af8b8
laviniabtista <143637409+laviniabtista@users.noreply.github.com>
2023-09-27 14:17:28 -0300

    Add files via upload

---
+++ SisAcademico/app/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from app.models import *
+
+# Register your models here.
+
+admin.site.register(Ocupacao)
+admin.site.register(Cidades)
+admin.site.register(Pessoas)
+admin.site.register(Instituicao)
+admin.site.register(AreasConhecimento)
+admin.site.register(Cursos)
+admin.site.register(Periodos)
+admin.site.register(Disciplinas)
+admin.site.register(Matricula)
+admin.site.register(TiposAvaliacao)
+admin.site.register(Avaliacoes)
+admin.site.register(Frequencia)
+admin.site.register(Turmas)
+admin.site.register(Ocorrencias)
+admin.site.register(DisciplinasCurso)
+
